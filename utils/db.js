e7ebde0814578d3e361e5d48dce7e2f67a21279b
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2025-03-04 17:31:59 +0530

    files updated

--- utils/db.js
+++ utils/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        // mongoose.set(true);
-        await mongoose.connect(process.env.MONGO_URI, {
+        mongoose.set("strictQuery", true);
+        await mongoose.connect(process.env.MONG_URI, {
            
         });
         console.log("MongoDB connected successfully");
