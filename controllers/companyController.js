918158dbf72113d1892e757f719ca874bf5fccd0
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2025-03-24 09:49:38 +0530

    updated

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -1,7 +1,7 @@
 
 import { Company } from "../models/companyModel.js"
-// import getDataUri from "../utils/datauri.js";
-// import cloudinary from "../utils/cloudinary.js";
+import getDataUri from "../utils/datauri.js";
+import cloudinary from "../utils/cloudinary.js";
 
 export const registerCompany = async (req, res) => {
     try {
@@ -72,13 +72,13 @@ export const getCompanyById = async (req, res) => {
 }
 export const updateCompany = async (req, res) => {
     try {
-        const { name, description, website, location, logo } = req.body;
+        const { name, description, website, location, } = req.body;
  
         const file = req.file;
       
-        // const fileUri = getDataUri(file);
-        // const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
-        // const logo = cloudResponse.secure_url;
+        const fileUri = getDataUri(file);
+        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+        const logo = cloudResponse.secure_url;
     
         const updateData = { name, description, website, location, logo };
 
