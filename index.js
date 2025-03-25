33f26f53e214e32739519fe64f4434d09a25c167
Dhineshkumar1202 <dhineshtrendy12@gmail.com>
2025-03-25 12:04:21 +0530

    Code Updated 1

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: "*",
+    origin: "https://cpms-portal-server-cpms1.vercel.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
 
